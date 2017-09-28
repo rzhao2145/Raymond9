0ed79a93524e91bf1df2aa270e9dd80fbedea66d
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-09-28 15:01:13 -0400

    9/28

--- src/chatbot/ChatbotMain.java
+++ src/chatbot/ChatbotMain.java
@@ -1,7 +1,7 @@
 package chatbot;
 
 public class ChatbotMain {
-
+	public static final Chatbot chatbow = new Chatbot();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
